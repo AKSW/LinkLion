e2d0fde10a4a4c9f0dcb98e64aa01b59ff98bbe1
mommi84 <mommi84@gmail.com>
2014-01-31 05:04:31 +0100

    design completed

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,8 +1,32 @@
-$(check());
-
+/* 
+ * TODO 
+ * check if everything is okay (framework, algorithm, new stuff, sources)
+ */
 function check() {
 	if($("#upload-file-info").html() != "" && $("#selected-framework").html() != "")
 		$("#submit-button").prop('disabled', false);
 	else
 		$("#submit-button").prop('disabled', true);
 }
+
+function new_framework(nf) {
+	if(nf) {
+		$('#new-framework').show();
+	} else {
+		$('#new-framework').hide();
+	}
+}
+
+function new_algorithm(nf) {
+	if(nf) {
+		$('#new-algorithm').show();
+	} else {
+		$('#new-algorithm').hide();
+	}
+}
+
+$(document).ready(function () {
+	$(check());
+	$(new_framework(false));
+	$(new_algorithm(false));
+});
