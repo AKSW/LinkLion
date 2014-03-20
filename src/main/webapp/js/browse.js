3b28d3164acf74fec42835b445682d55103e3908
Markus Nentwig <nentwig@informatik.uni-leipzig.de>
2014-03-20 17:44:53 +0100

    fixed #44

--- src/main/webapp/js/browse.js
+++ src/main/webapp/js/browse.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
-	$('#mappings-table').dynatable({
-		table: {
-			defaultColumnIdStyle: 'trimDash'
-		},
-		inputs: {
-			processingText: 'Loading <img src="../images/loading.gif" border="0"/>'
-		}
-	});
+        $('#mappings-table').dynatable({
+                table: {
+                        defaultColumnIdStyle: 'trimDash'
+                },
+                inputs: {
+                        processingText: 'Loading <img src="../images/loading.gif" border="0"/>'
+                },
+                readers: {
+                    'links': function(el, record) {
+                        return Number(el.innerHTML.replace(/,/g, ''));
+                    }   
+                },
+                writers: {
+                    'links': function(record) {
+                        return record.links.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                }
+        });
 });
